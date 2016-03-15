eeb1c334f9f278a3ee168bdc801b9926c48cb577
jeremiahscanlon <jeremiah.j.scanlon@gmail.com>
2016-03-15 00:16:54 -0400

    https

--- assets/js/testing.js
+++ assets/js/testing.js
@@ -1,4 +1,4 @@
-var queryURL = "http://api.indeed.com/ads/apisearch?publisher=8023780673544955&q=java&l=austin%2C+tx&useragent=Mozilla/%2F4.0%28Firefox%29&v=2"
+var queryURL = "https://api.indeed.com/ads/apisearch?publisher=8023780673544955&q=java&l=austin%2C+tx&v=2"
 
 // create ajax request to call API
 $.ajax({url: queryURL, method: 'GET',dataType: "xml"})
