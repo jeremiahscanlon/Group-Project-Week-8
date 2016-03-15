245e0869f07383048a25c9f45150d2dd1fbe66ff
jeremiahscanlon <jeremiah.j.scanlon@gmail.com>
2016-03-15 03:35:38 -0400

    changed map

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,7 +6,7 @@ var map;
   function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: 40.488, lng: -74.439},
-      zoom: 12
+      zoom: 10
     });
   }
 
