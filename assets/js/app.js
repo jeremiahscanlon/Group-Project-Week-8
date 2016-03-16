be04652e64f8536584de617453c9201e2f769870
jeremiahscanlon <jeremiah.j.scanlon@gmail.com>
2016-03-16 01:57:05 -0400

    added map, more indeed research

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,22 +2,96 @@
 // Google Maps API Key - AIzaSyBOo3mntkfMMomnO0V0P6Mt4bQ3vMUUWIw
 //==================================================================
 
-var map;
-  function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: 40.488, lng: -74.439},
-      zoom: 10
-    });
-  }
+// var map;
+// 	function initMap() {
+// 		var myLatLng = {lat: 40.488, lng: -74.439};
+
+// 		map = new google.maps.Map(document.getElementById('map'), {
+// 			center: myLatLng,
+// 			zoom: 10
+// 		});
+
+// 		// about markers: https://developers.google.com/maps/documentation/javascript/markers#introduction
+// 		var marker = new google.maps.Marker({
+// 			position: myLatLng,
+// 			map: map,
+// 			title: 'This that thing!'
+// 		});
+// 	}
+
+// stylized map info: https://developers.google.com/maps/documentation/javascript/styling#creating_a_styledmaptype
+function initMap() {
+
+	// Create an array of styles.
+	var styles = [
+		{
+			stylers: [
+				{ hue: "#0033cc" },
+				{ saturation: -20 }
+			]
+		},{
+			featureType: "road",
+			elementType: "geometry",
+			stylers: [
+				{ lightness: 100 },
+				{ visibility: "simplified" }
+			]
+		},{
+			featureType: "road",
+			elementType: "labels",
+			stylers: [
+				{ visibility: "off" }
+			]
+		}
+	];
+
+	// Create a new StyledMapType object, passing it the array of styles,
+	// as well as the name to be displayed on the map type control.
+	var styledMap = new google.maps.StyledMapType(styles,
+		{name: "Styled Map"});
+
+	// Create a map object, and include the MapTypeId to add
+	// to the map type control.
+	var mapOptions = {
+		zoom: 11,
+		center: new google.maps.LatLng(40.488, -74.439),
+		mapTypeControlOptions: {
+			mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'map_style']
+		}
+	};
+	var map = new google.maps.Map(document.getElementById('map'),mapOptions);
+
+	// var marker = new google.maps.Marker({
+	// 	position: myLatLng,
+	// 	map: map,
+	// 	title: 'This that thing!'
+	// });
+
+	//Associate the styled map with the MapTypeId and set it to display.
+	map.mapTypes.set('map_style', styledMap);
+	map.setMapTypeId('map_style');
+}
 
 //==================================================================
 // Indeed API request
 //==================================================================
 
-var searchLocation = "new+brunswick%2C+nj";
-var searchType = "node";
+$('seeResults').click(function(){
+	var keywords = $('#keywords').val().trim();
+	var home = $('#location').val().trim();
+	$('#keywords').val('');
+	$('#location').val('');
+});
 
-var queryURL = "http://api.indeed.com/ads/apisearch?publisher=8023780673544955&format=json&q="+searchType+"&l="+searchLocation+"&v=2";
+var home = 'New+Brunswick%2C+NJ';
+var keywords = 'node+full+stack+-python';
+var searchLocation = "&l="+home;
+var searchKeyword = "&q="+keywords;
+
+
+var queryURL = "http://api.indeed.com/ads/apisearch?publisher=8023780673544955&format=json"+searchKeyword+searchLocation+"&limit=10&v=2";
+
+console.log(queryURL);
 
 $.ajax({
 	url: queryURL,
@@ -28,8 +102,16 @@ $.ajax({
 .done(function(response) {
 	
 	//console.log(response);
+
 	var results = response.results
+	var numResults = response.totalResults;
+	var query = response.query;
+	var responseLocation = response.location;
+
+	$("#resultsList").html("<div class=\"searchHeader\"><h1>"+numResults+" Results for:<br>"+query+"<br>"+responseLocation+"</h1></div>");
+
 	for (var i = 0; i < results.length; i++) {
+		
 		var jobTitle = results[i].jobtitle;
 		var company = results[i].company;
 		var location = results[i].formattedLocationFull;
