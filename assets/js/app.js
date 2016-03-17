42e79c3f489c461bedc00946db36293c4ee361b2
jeremiahscanlon <jeremiah.j.scanlon@gmail.com>
2016-03-17 04:02:44 -0400

    search working

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,100 +2,20 @@
 // Google Maps API Key - AIzaSyBOo3mntkfMMomnO0V0P6Mt4bQ3vMUUWIw
 //==================================================================
 
+var map;
+function initMap() {
 
- var map;
- 	function initMap() {
- 		var myLatLng = {lat: 40.488, lng: -74.439};
+	var myLatLng = {lat: 40.488, lng: -74.439};
 
- 		map = new google.maps.Map(document.getElementById('map'), {
- 			center: myLatLng,
- 			zoom: 10
- 		});
+	var mapOptions = {
+		center: myLatLng,
+		zoom: 10,
+		styles: [{"featureType":"road","elementType":"geometry","stylers":[{"color":"#b1ab85"}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#8cc0c3"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#cde6cf"}]},{"featureType":"landscape.man_made","elementType":"geometry","stylers":[{"color":"#efede8"}]}]
+	};
 
- 		// about markers: https://developers.google.com/maps/documentation/javascript/markers#introduction
- 		var marker = new google.maps.Marker({
- 			position: myLatLng,
- 			map: map,
- 			title: 'This that thing!'
- 		});
- 	}
+	map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
-$('.resultsarea').on('click', '.searchResult', function(){
-	var cLat = $(this).data('lat');
-	var cLong = $(this).data('long');
-	var myLatLng = {lat: cLat, lng: cLong};
-	var cTitle = $(this).data('title');
-	var cCompany = $(this).data('company');
-		myLatlng = new google.maps.LatLng(cLat,cLong);
-				  
-						allMarkers = new google.maps.Marker({
-							position: myLatlng,
-							map: map,
-							title: cCompany
-						});
-   				
-   				var infowindow = new google.maps.InfoWindow({
-
-  				content: cCompany
-  				});
-
-		google.maps.event.addListener(allMarkers, 'click', function() {
-		  infowindow.open(map,allMarkers);
-  });
-
-    });
-
-// stylized map info: https://developers.google.com/maps/documentation/javascript/styling#creating_a_styledmaptype
-// function initMap() {
-
-// 	// Create an array of styles.
-// 	var styles = [
-// 		{
-// 			stylers: [
-// 				{ hue: "#0033cc" },
-// 				{ saturation: -20 }
-// 			]
-// 		},{
-// 			featureType: "road",
-// 			elementType: "geometry",
-// 			stylers: [
-// 				{ lightness: 100 },
-// 				{ visibility: "simplified" }
-// 			]
-// 		},{
-// 			featureType: "road",
-// 			elementType: "labels",
-// 			stylers: [
-// 				{ visibility: "off" }
-// 			]
-// 		}
-// 	];
-
-// 	// Create a new StyledMapType object, passing it the array of styles,
-// 	// as well as the name to be displayed on the map type control.
-// 	var styledMap = new google.maps.StyledMapType(styles,
-// 		{name: "Styled Map"});
-
-// 	// Create a map object, and include the MapTypeId to add
-// 	// to the map type control.
-// 	var mapOptions = {
-// 		zoom: 11,
-// 		center: new google.maps.LatLng(40.488, -74.439),
-// 		mapTypeControlOptions: {
-// 			mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'map_style']
-// 		}
-// 	};
-// 	var map = new google.maps.Map(document.getElementById('map'),mapOptions);
-
-
-
-// 	//Associate the styled map with the MapTypeId and set it to display.
-// 	map.mapTypes.set('map_style', styledMap);
-// 	map.setMapTypeId('map_style');
-// }
-
-
-
+}
 
 //==================================================================
 // Indeed API request
@@ -106,21 +26,61 @@ function initialSearch() {
 	
 	var keywords = $('#keywords').val().trim();
 	var home = $('#location').val().trim();
-	$('#resultsList').show();
 	$('#keywords').val('');
 	$('#location').val('');
+
+	$('.results').show();
+	$('#search').hide();
 	
-	var home = 'New+Brunswick%2C+NJ';
-	var keywords = 'node';
+	//var home = 'New+Brunswick%2C+NJ';
+	//var keywords = 'node';
 	var searchLocation = "&l="+home;
 	var searchKeyword = "&q="+keywords;
 	var page = "&limit=";
 	var pagenum = 10;
 	var first = true;
 
+	getCenter(home);
 	buildResults(searchKeyword,searchLocation,page,pagenum,first);
 
 }
+
+function getCenter(home) {
+	var centerLocation = home;
+
+	var requestURL = 'https://maps.googleapis.com/maps/api/geocode/json?address='+centerLocation+'&key=AIzaSyBOo3mntkfMMomnO0V0P6Mt4bQ3vMUUWIw';
+	
+	console.log(requestURL);
+
+	$.ajax({
+		url: requestURL,
+		method: 'GET',
+	})
+	.done(function(response) {
+		console.log(response);
+		var results = response.results[0].geometry.location;
+		var centerLat = results.lat;
+		var centerLong = results.lng;
+
+		console.log('lat: '+centerLat);
+		console.log('long: '+centerLong);
+		initMap();
+		reCenter(centerLat, centerLong);
+	});
+}
+
+function reCenter(lat, long) {
+	var center = new google.maps.LatLng(lat, long);
+	map.panTo(center);
+
+	var marker = new google.maps.Marker({
+		position: center,
+		map: map,
+		title: 'This is your Search Center',
+		icon: 'assets/images/arrow.png'
+	});
+}
+
 
 function buildResults(searchKeyword,searchLocation,page,pagenum,first) {
 	
@@ -131,9 +91,7 @@ function buildResults(searchKeyword,searchLocation,page,pagenum,first) {
 	var initialSearch = first;
 
 	var queryURL = "http://api.indeed.com/ads/apisearch?publisher=8023780673544955&format=json"+searchKey+searchLoc+pag+pageNumber+"&v=2";
-	console.log(queryURL);
-
-	console.log(pageNumber);
+	//console.log(queryURL);
 
 	$.ajax({
 		url: queryURL,
@@ -143,7 +101,7 @@ function buildResults(searchKeyword,searchLocation,page,pagenum,first) {
 	})
 	.done(function(response) {
 		
-		//console.log(response);
+		console.log(response);
 
 		var results = response.results
 		var numResults = response.totalResults;
@@ -177,15 +135,45 @@ function buildResults(searchKeyword,searchLocation,page,pagenum,first) {
 				var lat = response.results[0].latitude;
 				var jobKey = response.results[0].jobkey;
 				var jobTitle = response.results[0].jobtitle;
+				var link = response.results[0].url;
+				var cCompany = response.results[0].company;
 
 				$("#"+jobKey).append("<p>" + lat +", "+ long + "</p>");
 				$("#"+jobKey).attr('data-lat', lat);
 				$("#"+jobKey).attr('data-long', long);
 				$("#"+jobKey).attr('data-company', jobTitle);
+			
+				var myLatlng = new google.maps.LatLng(lat,long);
+							  
+				allMarkers = new google.maps.Marker({
+					position: myLatlng,
+					map: map,
+					title: jobTitle
+				});
+			   	
+				var contentString = '<div id="content">'+
+										'<div id="siteNotice">'+
+										'</div>'+
+										'<h3 id="firstHeading" class="firstHeading">'+jobTitle+'</h3>'+
+										'<div id="bodyContent">'+
+											'<p><b>'+company+'</b></p>'+
+											'<a href="'+link+'" target="_blank">'+
+											'Job Details</a> '+
+										'</div>'+
+									'</div>';
+
+				var infowindow = new google.maps.InfoWindow({
+					content: contentString
+				});
+
+				google.maps.event.addListener(allMarkers, 'click', function() {
+					infowindow.open(map,allMarkers);
+				});
+
 			});			
 		}
 
-		if (numResults > 10 && numResults > pageNumber){
+		if (numResults > 10 && numResults >= pageNumber){
 			$("#resultsList").append("<button type=\"button\" class=\"btn btn-default center-block\" id=\"nextPage\">Next 10 <span class=\"glyphicon glyphicon-chevron-right\" aria-hidden=\"true\"></span></button>");
 			
 			$('#nextPage').click(function(){
@@ -200,18 +188,17 @@ function buildResults(searchKeyword,searchLocation,page,pagenum,first) {
 					console.log(searchKey +" - "+ searchLoc +" - "+ start +" - " + pageNumber+" - " + initialSearch);
 					buildResults(searchKey,searchLoc,start,pageNumber,first);				
 				}
-				
-
-			});
-			
+			});	
 		}
-
 
 	});
 }
 
-initialSearch();
+//initialSearch();
 
 $('#seeResults').click(function(){
 	initialSearch();
+
+	// Don't refresh the page!
+	return false;
 });
